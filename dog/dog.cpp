f7069bc416a654366700a96482c0ee03a6b6ab50
Mooooon-s <mmk990212@naver.com>
2021-07-04 17:08:17 +0900

    Today i solve three qustions

---
+++ dog/dog.cpp
@@ -0,0 +1,20 @@
+/*
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+    cout<<"|\\_/|"<<endl;
+    cout<<"|q p|   /}"<<endl;
+    cout<<"( 0 )\"\"\"\\"<<endl;
+    cout<<"|\"^\"`    |"<<endl;
+    cout<<"||_/=\\\\__|"<<endl;
+    return 0;
+}
