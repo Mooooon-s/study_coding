03149ddb284348e45687ea1c7de9e628f1a1f0ad
Mooooon-s <mmk990212@naver.com>
2023-12-15 18:58:58 +0900

    solve 2164

--- 2164/2164.cpp
+++ 2164/2164.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 int main(void)
 {
+    queue<int> a;
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+    {
+        a.push(i);
+    }
 
-    return 0;
+    //제일 위에 있는걸 뺌
+    //제일 위에 있는걸 아래로
+
+    while (a.size() != 1)
+    {
+        a.pop();
+
+        if (a.size() == 1)
+            break;
+
+        a.push(a.front());
+        a.pop();
+    }
+
+    cout << a.front();
 }
