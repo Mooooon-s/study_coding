d5d929e9fad7f2ce48e22fa55acc7d47e5ad31ca
Mooooon-s <64954014+Mooooon-s@users.noreply.github.com>
2024-04-29 16:19:43 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.cc
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -4,7 +4,6 @@
 using namespace std;
 
 vector<pair<int,int>> h;
-vector<pair<int,int>> t;
 
 int main() {
     int n;
@@ -20,28 +19,16 @@ int main() {
     
     for(int i=0;i<n;i++)
         {
-            int a=0;
-            int b=0;
+            int a=1;
             for(int j=0;j<n;j++)
                 {
                     if(i==j)
                         continue;
-                    if(h[i].first > h[j].first && h[i].second > h[j].second )
+                    
+                    if(h[i].first < h[j].first && h[i].second < h[j].second )
                         a++;
-                    else if(h[i].first < h[j].first && h[i].second < h[j].second )
-                        continue;
-                    else
-                    {
-                        b++;    
-                    }
                 }
-            t.push_back(make_pair(a,b));
+            cout << a << " ";
         }
-
-    for(auto tier : t)
-        {
-            cout << n - tier.first - tier.second << " ";
-        }
-
     return 0;
 }
