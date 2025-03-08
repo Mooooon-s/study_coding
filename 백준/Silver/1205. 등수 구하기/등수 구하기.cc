d2f7c0925c338477d46ee77b945c7c9b1e3f878b
Mooooon-s <64954014+Mooooon-s@users.noreply.github.com>
2025-03-08 23:25:59 +0900

    [Silver IV] Title: 등수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool com(int a , int b)
+{
+    return a > b;
+}
+
+map<int,int,greater<int>> m;
+
+int main(void){
+    int n,high,less;
+    cin >> n >> high >> less;
+    
+    int* score= new int[n];
+    for(int i=0;i<n;i++){
+        cin >> score[i];
+    }
+
+    int rank=1;
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        if(score[i]==high){
+            rank=rank;
+        }
+        else if(score[i]> high){
+            rank+=1;
+        }
+        else break;
+        cnt++;
+    }
+
+    if(cnt==less) rank=-1;
+    if(n==0) rank=1;
+
+    cout << rank;
+
+    
+    return 0;
+}
